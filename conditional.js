299e569fa0de28f29787408e6bb89e985251379f
arjunesarkar <arjunesarker.bd@gmail.com>
2023-06-15 21:51:44 +0600

    bs project

---
+++ conditional.js
@@ -0,0 +1,25 @@
+var biscutePrice = 12
+if(biscutePrice<15){
+    console.log('I will eat biscute')
+}
+else{
+    console.log('tea is best')
+}
+
+var jobPaiso = true
+var savins = 50000
+if(jobPaiso==true && savins>200000){
+    console.log('aso patri khuji')
+}
+else if(jobPaiso==true){
+    console.log('koidin por patri khujbo')
+}
+else{
+    console.log('tor kopale biya nai')
+}
+
+
+
+
+var date = new Date('1967-02-21')
+console.log(date)
---
+++ index.js
@@ -0,0 +1,28 @@
+var bananaPrise = 10;
+console.log(bananaPrise)
+var seenAfter = 100;
+console.log(typeof seenAfter);
+var name = "Arjune sarkar";
+console.log(typeof name);
+var student = true;
+var teacher = false;
+console.log(typeof student);
+var promice = 'I want to promice you, i will never leave you.'
+console.log(promice.toLocaleUpperCase());
+var college = 'I am a diploma Engineere'
+console.log(college.indexOf('diploma'))
+var number1 = 20;
+var number2 ='35.5';
+number2 = parseInt(number2);
+console.log(number1+number2);
+var number3 = 0.3;
+var number4 = 0.6;
+var total = number3+number4;
+total= total.toFixed(3);
+console.log(total);
+var number = +25;
+var absuletNumber = Math.abs(number);
+console.log(absuletNumber)
+var number = 5.4999;
+var result = Math.floor(number);
+console.log(result);
